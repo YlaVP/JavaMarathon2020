0de30640800f6bff5fdae205c8750fed796c37a1
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2020-11-30 22:36:32 +0900

    Task 1

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        int k = 1;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + " * " + 1 + " = "  +1*k);
+        }
 
     }
 }
